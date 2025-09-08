31fd38ff0ec3febab376f45435fe90b9c9a3d57c
Pedro Rodrigues Almeida <pedrominato23@gmail.com>
2025-09-08 13:06:06 -0300

    Commit de teste

--- NodeAPI/server.js
+++ NodeAPI/server.js
@@ -4,4 +4,14 @@ const PORT = 3000
 
 app.listen(PORT, () => {
     console.log("Server escutando")
+    console.log("teste")
+    console.log("teste")
+    console.log("teste")
+    console.log("teste")
+    console.log("teste")
+    console.log("teste")
+    console.log("teste")
+    console.log("teste")
+
+
 })
