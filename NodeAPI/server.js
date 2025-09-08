ba03fcf881f665d35c3321d615166e95035bea74
Pedro Rodrigues Almeida <pedrominato23@gmail.com>
2025-09-08 13:02:51 -0300

    Criando registros com POST

--- NodeAPI/server.js
+++ NodeAPI/server.js
@@ -2,16 +2,6 @@ import app from "./src/app.js";
 
 const PORT = 3000
 
-const rotas = {
-    "/" : "Curso de node.js",
-    "/sobre" : "Sobre o curso"
-}
-
-// const server = http.createServer((req, res) => {
-//     res.writeHead(200 ,{"Content-Type" : "text/plain"})
-//     res.end(rotas[req.url])
-// })
-
 app.listen(PORT, () => {
     console.log("Server escutando")
 })
--- NodeAPI/src/app.js
+++ NodeAPI/src/app.js
@@ -1,9 +1,31 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
+
+const livros= [
+    {
+        id: 1,
+        titulo: "Senhor dos Aneis",
+    },
+    {
+        id: 2,
+        titulo: "Harry Potter",
+    },
+]
 
 app.get("/", (req,res)=> {
     res.status(200).send("Curso de node.js")
 });
+
+app.get("/livros" , (req,res) => {
+    res.status(200).json(livros);
+});
+
+app.post("/livros", (req,res) => {
+    livros.push(req.body);
+    res.status(201).send("livro cadastrado com sucesso")
+});
+
 
 export default app;
