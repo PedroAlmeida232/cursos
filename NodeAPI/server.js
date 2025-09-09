c88540f291a6e81930ea89ed02e4c40925e095c1
Pedro Rodrigues Almeida <pedrominato23@gmail.com>
2025-09-09 04:57:42 -0300

    Ultilizando CRUD na API

--- NodeAPI/server.js
+++ NodeAPI/server.js
@@ -4,14 +4,4 @@ const PORT = 3000
 
 app.listen(PORT, () => {
     console.log("Server escutando")
-    console.log("teste")
-    console.log("teste")
-    console.log("teste")
-    console.log("teste")
-    console.log("teste")
-    console.log("teste")
-    console.log("teste")
-    console.log("teste")
-
-
 })
--- NodeAPI/src/app.js
+++ NodeAPI/src/app.js
@@ -10,22 +10,45 @@ const livros= [
     },
     {
         id: 2,
-        titulo: "Harry Potter",
+        titulo: "Tropa de elite",
     },
 ]
 
-app.get("/", (req,res)=> {
+function buscalivro(id) {
+    return livros.findIndex(livro => {
+        return livro.id === Number(id)
+    })
+}
+
+
+app.get("/", (req, res)=> {
     res.status(200).send("Curso de node.js")
 });
 
-app.get("/livros" , (req,res) => {
+app.get("/livros" , (req, res) => {
     res.status(200).json(livros);
 });
 
-app.post("/livros", (req,res) => {
+app.get("/livros/:id", (req, res) => {
+    const index = buscalivro(req.params.id)
+    res.status(200).json(livros[index])
+})
+
+app.post("/livros", (req, res) => {
     livros.push(req.body);
     res.status(201).send("livro cadastrado com sucesso")
 });
 
+app.put("/livros/:id", (req, res) => {
+    const index = buscalivro(req.params.id)
+    livros[index].titulo = req.body.titulo
+    res.status(200).json(livros)
+})
+
+app.delete("/livros/:id", (req, res) =>{
+    const index = buscalivro(req.params.id)
+    livros.splice(index, 1)
+    res.status(200).send("livro removido com sucesso")
+})
 
 export default app;
